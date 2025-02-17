fbc57f41aef6c23b1a0e1d2389a2c5d8f22b3e21
yichatani <yichatma@gmail.com>
2025-02-17 17:14:51 +0800

    Update _hdf5_.py

--- scripts/_hdf5_.py
+++ scripts/_hdf5_.py
@@ -1,16 +1,32 @@
 import h5py
 
-h5_file_path = "/media/mldadmin/home/s124mdg32_04/Dataset/mnt/data1/data_ifl/scene20/2_scene.hdf5"
+ROOT_DIR = "/home/ani/astar/my_Isaac/episodes"
 
 # Read structure
-# with h5py.File(h5_file_path, 'r') as f:
-#     def print_hdf5_structure(name, obj):
-#         print(name)
-#     f.visititems(print_hdf5_structure)
+def read_structure(path):
+    """
+    Read the data structure of h5 files.
+    """
+    with h5py.File(path, 'r') as f:
+        def print_hdf5_structure(name, obj):
+            print(name)
+        f.visititems(print_hdf5_structure)
 
 
-with h5py.File(h5_file_path, 'r') as f:
-    
-    dset = f['non_colliding_grasps/paralleljaw/contact_width']
-    print("Dataset shape:", dset.shape)
-    print("First 10 values:", dset[:300]) 
+def read_values(path, key):
+    """
+    Read the key values. 
+    """
+    with h5py.File(path, 'r') as f:
+        dset = f[key]
+        print("Dataset shape:", dset.shape)
+        print("First 10 values:", dset[:5])
+
+if __name__ == "__main__":
+
+    path = ROOT_DIR + "/episode_7.h5"
+    read_structure(path)
+    print("======")
+    read_values(path, "agent_pos")
+    print("======")
+    read_values(path, "action")
